69898d7db1b191fe0deb9a66a4c4c1d3eac93e7a
Cagdas Polat <51061196+ekimdusu@users.noreply.github.com>
2022-09-17 12:24:34 +0300

    v0.4

--- app.js
+++ app.js
@@ -14,17 +14,17 @@ http.createServer(function (req, res) {
     });
     res.end(html);
   }).listen(8080);*/
-// const uri = "mongodb+srv://webperfix:7t24xz@cluster0.pdh03oe.mongodb.net/?retryWrites=true&w=majority";
-// const client = new MongoClient(uri);
-// async function run() {
-//     try {
-//       await client.connect();
-//       client.db("webperfix").command({ ping: 1 });
-//       var db =  client.db("webperfix");
-//       console.log("Connected successfully to server");
-//       // read many
-//       // const cursor = db.collection('domain').find({});
-//       // await cursor.forEach(doc => console.log(doc));
+const uri = "mongodb+srv://webperfix:7t24xz@cluster0.pdh03oe.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
+async function run() {
+    try {
+      await client.connect();
+      client.db("webperfix").command({ ping: 1 });
+      var db =  client.db("webperfix");
+      console.log("Connected successfully to server");
+      // read many
+       const cursor = db.collection('domain').find({});
+      await cursor.forEach(doc => console.log(doc));
 
 //       // delete object
 //       /*const filter = { name: "Jumbo"};
@@ -46,40 +46,40 @@ http.createServer(function (req, res) {
 //       const result = await db.collection('settings').insertOne(doc);
 //       console.log(result);*/
 
-//     } finally {
-//       // Ensures that the client will close when you finish/error
-//       await client.close();
-//     }
-//   }
-//   run().catch(console.dir);
-  (async () => {
-    /*const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
-    const options = {logLevel: 'info', output: 'json', onlyCategories: ['performance'], port: chrome.port};
-    const runnerResult = await lighthouse('https://www.neredeoku.com', options);
+     } finally {
+      // Ensures that the client will close when you finish/error
+   await client.close();
+   }
+ }
+   run().catch(console.dir);
+  // (async () => {
+  //   /*const chrome = await chromeLauncher.launch({chromeFlags: ['--headless']});
+  //   const options = {logLevel: 'info', output: 'json', onlyCategories: ['performance'], port: chrome.port};
+  //   const runnerResult = await lighthouse('https://www.neredeoku.com', options);
   
-    // `.report` is the HTML report as a string
-    // const reportHtml = runnerResult.report;
-    // fs.writeFileSync('lhreport.html', reportHtml);
-    fs.writeFileSync('lhreport.json', JSON.stringify(runnerResult.lhr));
+  //   // `.report` is the HTML report as a string
+  //   // const reportHtml = runnerResult.report;
+  //   // fs.writeFileSync('lhreport.html', reportHtml);
+  //   fs.writeFileSync('lhreport.json', JSON.stringify(runnerResult.lhr));
   
-    // `.lhr` is the Lighthouse Result as a JS object
-    console.log('Report is done for', runnerResult.lhr);
-    console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
+  //   // `.lhr` is the Lighthouse Result as a JS object
+  //   console.log('Report is done for', runnerResult.lhr);
+  //   console.log('Performance score was', runnerResult.lhr.categories.performance.score * 100);
 
-    await chrome.kill();*/
+  //   await chrome.kill();*/
 
-    const wpt = new WebPageTest("https://www.webpagetest.org", "98681b86-eaae-4609-981a-c00fd5b4dd6a");
-    wpt.runTest("https://www.neredeoku.com", (err, data) => {
-      console.log(err || data);
-      //wpt.getTestStatus("220914_AiDc28_AHY", (err, res) => {
-      wpt.getTestResults("220914_AiDc28_AHY", (err, res) => {
-        console.log(err || res);
-        fs.writeFileSync('lhreport2.json', JSON.stringify(res));
-      });
-    });
+  //   const wpt = new WebPageTest("https://www.webpagetest.org", "98681b86-eaae-4609-981a-c00fd5b4dd6a");
+  //   wpt.runTest("https://www.neredeoku.com", (err, data) => {
+  //     console.log(err || data);
+  //     //wpt.getTestStatus("220914_AiDc28_AHY", (err, res) => {
+  //     wpt.getTestResults("220914_AiDc28_AHY", (err, res) => {
+  //       console.log(err || res);
+  //       fs.writeFileSync('lhreport2.json', JSON.stringify(res));
+  //     });
+  //   });
     
   
-  })();
+  // })();
   /*function buildHtml(req) {
     var header = '';
     var body = '<h1>Hello World!!</h1>';
